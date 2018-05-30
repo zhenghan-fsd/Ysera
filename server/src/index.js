8eb3f8b88cd5d11f27eafc98ea0efc12733d11ae
Zheng Han <zhenghan@catail.cn>
2018-05-30 10:10:57 +0800

    graphql-server with dummy data

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,9 @@
 import express from "express";
+import graphqlMiddleware from "express-graphql";
+import schema from "./schema/schema";
 
 const app = express();
+
+app.use("/graphql", graphqlMiddleware({ schema, graphiql: true }));
 
 app.listen(3001, () => console.info("Listening on port: 3001"));
